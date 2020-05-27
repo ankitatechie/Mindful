b89f0e6f3e0157f6932460fb39d861eeb4b63c5a
Jayesh Bidani <jayesh.bidani@gmail.com>
2020-05-28 00:41:20 +0530

    snow working when selected from popup menu

--- background.js
+++ background.js
@@ -5,9 +5,7 @@
 "use strict";
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: "#3aa757" }, function() {
-    console.log("The color is green.");
-  });
+  console.log("Extension is ready to use!");
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
