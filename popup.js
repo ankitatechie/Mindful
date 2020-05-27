d6196d27503532762ff101d6cd995383c1203dbb
Jayesh Bidani <jayesh.bidani@gmail.com>
2020-05-28 02:12:04 +0530

    snow and rain added, but code needs refactoring on a major level

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@
 "use strict";
 
 let showSnow = document.getElementById("show-snow");
+let showRain = document.getElementById("show-rain");
 
 function addSnow() {
   chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
@@ -12,7 +13,14 @@ function addSnow() {
     chrome.tabs.sendMessage(activeTab.id, { message: "runSnow" });
   });
 }
+function addRain() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { message: "runRain" });
+  });
+}
 document.addEventListener("DOMContentLoaded", function() {
   showSnow.addEventListener("click", addSnow);
+  showRain.addEventListener("click", addRain);
   // chrome.tabs.sendMessage(null, { message: "changeBackground" });
 });
