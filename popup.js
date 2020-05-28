964022a308df76650fe96f833b684c681d04944a
Jayesh Bidani <jayesh.bidani@gmail.com>
2020-05-29 00:21:24 +0530

    sound added, minor refactoring done

--- popup.js
+++ popup.js
@@ -4,23 +4,36 @@
 
 "use strict";
 
-let showSnow = document.getElementById("show-snow");
-let showRain = document.getElementById("show-rain");
+function runAnimation(file) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.executeScript(activeTab.id, { file });
+  });
+}
 
-function addSnow() {
-  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: "runSnow" });
+function playSound(file, play = true) {
+  chrome.runtime.sendMessage({
+    type: "playSound",
+    options: {
+      file: file,
+      play: play
+    }
   });
 }
-function addRain() {
-  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: "runRain" });
-  });
-}
+
 document.addEventListener("DOMContentLoaded", function() {
-  showSnow.addEventListener("click", addSnow);
-  showRain.addEventListener("click", addRain);
-  // chrome.tabs.sendMessage(null, { message: "changeBackground" });
+  let showSnow = document.getElementById("show-snow");
+  let showRain = document.getElementById("show-rain");
+  let playRainSound = document.getElementById("play-rain");
+
+  showSnow.addEventListener("click", () =>
+    runAnimation("particles/js/snow.js")
+  );
+  showRain.addEventListener("click", () =>
+    runAnimation("particles/js/rain.js")
+  );
+
+  playRainSound.addEventListener("click", () =>
+    playSound(chrome.runtime.getURL("audios/om.mp3"), true)
+  );
 });
