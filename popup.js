9913ffc8dcdd82a20bf16d46ff71c6ea189064c3
Jayesh Bidani <jayesh.bidani@gmail.com>
2020-05-29 16:03:34 +0530

    code merging, audio , rain and snow added

--- popup.js
+++ popup.js
@@ -1,11 +1,77 @@
 "use strict";
 
-function runAnimation(file, cssFileName = false) {
+const data = [
+  {
+    name: "rain",
+    audio: {
+      btnId: "play-rain",
+      eventName: "toggleAudio",
+      file: "audios/rain.mp3"
+    },
+    graphic: {
+      btnId: "show-rain",
+      eventName: "toggleGraphic",
+      file: "particles/js/rain.js",
+      cssFile: "particles/css/rain.css"
+    }
+  },
+  {
+    name: "snow",
+    audio: {
+      btnId: "play-snow",
+      eventName: "toggleAudio",
+      file: "audios/snow.mp3"
+    },
+    graphic: {
+      btnId: "show-snow",
+      eventName: "toggleGraphic",
+      file: "particles/js/snow.js"
+    }
+  },
+  {
+    name: "stars",
+    audio: {
+      btnId: "play-stars",
+      eventName: "toggleAudio",
+      file: "audios/snow.mp3"
+    },
+    graphic: {
+      btnId: "show-stars",
+      eventName: "toggleGraphic"
+    }
+  },
+  {
+    name: "om",
+    audio: {
+      btnId: "play-om",
+      eventName: "toggleAudio",
+      file: "audios/om.mp3"
+    },
+    graphic: {
+      btnId: "show-om",
+      eventName: "toggleGraphic"
+    }
+  },
+  {
+    name: "piano",
+    audio: {
+      btnId: "play-piano",
+      eventName: "toggleAudio",
+      file: "audios/piano.mp3"
+    },
+    graphic: {
+      btnId: "show-piano",
+      eventName: "toggleGraphic"
+    }
+  }
+];
+
+function runAnimation(file, cssFile = false) {
   chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.executeScript(activeTab.id, { file });
-    if (cssFileName) {
-      chrome.tabs.insertCSS((activeTab.id, { file: cssFileName }));
+    if (cssFile) {
+      chrome.tabs.insertCSS((activeTab.id, { file: cssFile }));
     }
   });
 }
@@ -21,18 +87,19 @@ function playSound(file, play = true) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  let showSnow = document.getElementById("show-snow");
-  let showRain = document.getElementById("show-rain");
-  let playRainSound = document.getElementById("play-rain");
+  for (let obj of data) {
+    const playBtn = document.getElementById(obj.audio.btnId);
+    const graphicBtn = document.getElementById(obj.graphic.btnId);
+    if (playBtn) {
+      playBtn.addEventListener("click", e => {
+        playSound(chrome.runtime.getURL(obj.audio.file), true);
+      });
+    }
 
-  showSnow.addEventListener("click", () =>
-    runAnimation("particles/js/snow.js")
-  );
-  showRain.addEventListener("click", () =>
-    runAnimation("particles/js/rain.js", "particles/css/rain.css")
-  );
-
-  playRainSound.addEventListener("click", () =>
-    playSound(chrome.runtime.getURL("audios/om.mp3"), true)
-  );
+    if (graphicBtn) {
+      graphicBtn.addEventListener("click", e => {
+        runAnimation(obj.graphic.file, obj.graphic.cssFile);
+      });
+    }
+  }
 });
